106b56bcb85e8d58fdd0831c58da2dcdc733ccfe
jessigu <jessigu@gmail.com>
2019-04-15 19:28:25 -0700

    apartment component working

--- app/javascript/components/AuthenticatedApp.js
+++ app/javascript/components/AuthenticatedApp.js
@@ -2,13 +2,20 @@ import React from "react"
 import PropTypes from "prop-types"
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
+import Apartments from './pages/Apartments'
+
 class AuthenticatedApp extends React.Component {
   render () {
     return (
-      <React.Fragment>
-        <div>Authenticated</div>
-        <a rel="nofollow" data-method="delete" href="/users/sign_out">Logout</a>
-      </React.Fragment>
+      <Router>
+        <div>
+          <Link to="/apartments">See All Apartments</Link><br />
+          <a rel="nofollow" data-method="delete" href="/users/sign_out">Logout</a>
+          <Switch>
+            <Route path="/apartments" component={Apartments} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
